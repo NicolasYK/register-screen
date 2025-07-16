1786d8bbd355807f0240fb397aba9040f232faab
NicolasYK <kosaka.nicolas@gmail.com>
2025-07-16 16:53:51 -0300

    Condicao de senhas

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,17 +14,51 @@ class ValidateForm {
     }
 
     #checkField(){
+        this.#removeAlertField();
+        let valid = true;
+        let passwordName;
+        let firstPassword;
+        let confirmPassword;
+
         for(let field of this.#form.querySelectorAll('input')){
+            const label = field.previousElementSibling.innerText;
+            // Se os campos forem nulos
             if(!field.value){
-                this.#createAlert(field);
+                this.#createAlert(field, `O campo "${label}" é obrigatório!`);
+                valid = false;
+            }
+
+            if(field.classList.contains('input-cpf')){
+                
+                valid = false;
+            }
+            
+            if(field.classList.contains('input-password')){
+                firstPassword = field.value;
+                passwordName = label;
+                if(firstPassword.length < 12){
+                    this.#createAlert(field, `É necessário digitar no mínimo 12 letras.`);
+                }
+            }
+
+            if(field.classList.contains('input-validation-password')){
+                confirmPassword = field.value;
+                if(firstPassword != confirmPassword){
+                    this.#createAlert(field, `Os campos "${passwordName}s" precisam ser iguais.`)
+                }
             }
         }
     }
 
-    #createAlert(name_value){
+    #removeAlertField(){
+        for(let invalidText of this.#form.querySelectorAll('.invalid-field')){
+            invalidText.remove();
+        }
+    }
+
+    #createAlert(name_value, message){
         const p = document.createElement('p');
-        let labelTitle = name_value.previousElementSibling.innerHTML;
-        p.innerHTML = `O campo ${labelTitle} não pode estar em branco`;
+        p.innerHTML = message;
         p.classList.add('invalid-field');
         name_value.insertAdjacentElement('afterend', p);
     }
